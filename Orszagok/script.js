de2560b6922348ab1d2d8e700f5893303f67989c
szebbeni <szebenyi.benjamindaniel@simonyiszki.org>
2025-05-05 11:16:16 +0200

    Css és kártyák megjelenítése

--- Orszagok/script.js
+++ Orszagok/script.js
@@ -6,5 +6,34 @@ fetch('https://api.sampleapis.com/countries/countries')
             countries[`country_${country.id}`] = country;
         });
         console.log(countries);
+        // betöltés függvény hívás
+        loadCard(countries);
     })
     .catch(error => console.log('Hiba: ', error));
+
+
+function loadCard(countries) {
+    const cardContainer = document.getElementById('card-container');
+
+    if(!countries || Object.keys(countries).length === 0) {
+        console.error('Hiba: Nincsenek országok!');
+        return;
+    }
+
+    // Végéig lépkedünk az országokon
+    for (const key in countries) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+            <h2>${countries[key].name}</h2>
+            <img src="${countries[key].media.flag}" alt="${countries[key].name}_zaszlo">
+            <p><strong>Főváros</strong> ${countries[key].capital}</p>
+            <p><strong>Pénznem</strong> ${countries[key].currency}</p>
+            <p><strong>Rövidítés</strong> ${countries[key].abbreviation}</p>
+            `;
+
+            // kártya hozzáadása a konténerhez
+            cardContainer.appendChild(card);
+    }
+
+}
