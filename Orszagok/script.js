51a23766b9da8e661ce4e8d888cad6f2f598f395
szebbeni <szebenyi.benjamindaniel@simonyiszki.org>
2025-05-05 09:54:11 +0200

    Fetch

---
+++ Orszagok/script.js
@@ -0,0 +1,10 @@
+fetch('https://api.sampleapis.com/countries/countries')
+    .then(response => response.json())
+    .then(data => {
+        const countries = {};
+        data.forEach((country, index) => {
+            countries['country_${country.id}'] = country;
+        });
+        console.log(countries);
+    })
+    .catch(error => console.log('Hiba: ', error));
